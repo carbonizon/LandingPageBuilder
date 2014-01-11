cd230f7c90cfb829cfab93b604fe7e410304bb67
carbonizon <chriskanze@gmail.com>
2014-01-11 03:57:30 -0500

    FINAL

--- js/builder.js
+++ js/builder.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     //Startup Source
-    $('head').append('<style id="blackbg">body {background-image: url("images/bg.png");overflow-x: hidden;}</style>')
+    $('head').append('<style id="blackbg">body {background-color: rgba(1,1,1,0.8);}</style>')
     $('.lp').hide();
+    $('.source').hide();
     $('#builder').addClass('activebutton');
     $('.source').hide();
     $('.lphead').replaceWith('');
@@ -54,6 +55,7 @@ $(document).ready(function(){
     //Navigation-Switch
     $('#builder').click(function(){
         $(this).addClass('activebutton');
+        $('.title').replaceWith('<title class="title">Carbonizon</title>')
         $('#lp').removeClass('activebutton');
         $('#source').removeClass('activebutton');
         $('.builder').show();
@@ -61,7 +63,7 @@ $(document).ready(function(){
         $('.source').hide();
         $('.lp').hide();
         if ($('#blackbg').length === 0) {
-            $('head').append('<style id="blackbg">body {background-image: url("images/bg.png");overflow-x: hidden;}</style>')
+            $('head').append('<style id="blackbg">body {background-color: rgba(1,1,1,0.8);}</style>')
         }
         else {
             
@@ -76,14 +78,16 @@ $(document).ready(function(){
         var bodyheader1 = document.getElementById("bodyheader1").value;
         var imageurl1 = document.getElementById("imageurl1").value;
         var bodycontent1 = document.getElementById("bodycontent1").value;
-        if (bodyparagraph != 1) {
-            $('.bodycontent2').remove();
-            $('.bodycontentdivs').replaceWith('<div class="bodycontentdivs">div class="bodycontent1"><div class="row"><div class="body1 col-sm-6"><h3 class="bodyheader">'+bodyheader1+'</h3><p class="bodycontent">'+bodycontent1+'</p></div><div class="body1 col-sm-6"><img class="img-responsive bodyimage" src="'+imageurl1+'"></div></div></div><div class="bodycontent2"><div class="row"><div class="body2 col-sm-6"><img class="img-responsive bodyimage" src="'+imageurl2+'"></div><div class="body2 col-sm-6"><h3 class="bodyheader">'+bodyheader2+'</h3><p class="bodycontent2">'+bodycontent2+'</p></div></div></div></div>')
-        }
-        else {
-            $('.bodycontent2').remove();
-            $('.bodycontentdivs').replaceWith('<div class="bodycontentdivs"><div class="bodycontent1"><div class="row"><div class="body1 col-sm-6"><h3 class="bodyheader">'+bodyheader1+'</h3><p class="bodycontent">'+bodycontent1+'</p></div><div class="body1 col-sm-6"><img class="img-responsive bodyimage" src="'+imageurl1+'"></div></div></div></div>')
-            
+        if (bodyheader1.length > 0 || imageurl1.length > 0 || bodycontent1.length > 0) {
+            if (bodyparagraph != 1) {
+                $('.bodycontent2').remove();
+                $('.bodycontentdivs').replaceWith('<div class="bodycontentdivs">div class="bodycontent1"><div class="row"><div class="body1 col-sm-6"><h3 class="bodyheader">'+bodyheader1+'</h3><p class="bodycontent">'+bodycontent1+'</p></div><div class="body1 col-sm-6"><img class="img-responsive bodyimage" src="'+imageurl1+'"></div></div></div><div class="bodycontent2"><div class="row"><div class="body2 col-sm-6"><img class="img-responsive bodyimage" src="'+imageurl2+'"></div><div class="body2 col-sm-6"><h3 class="bodyheader">'+bodyheader2+'</h3><p class="bodycontent2">'+bodycontent2+'</p></div></div></div></div>')
+            }
+            else {
+                $('.bodycontent2').remove();
+                $('.bodycontentdivs').replaceWith('<div class="bodycontentdivs"><div class="bodycontent1"><div class="row"><div class="body1 col-sm-6"><h3 class="bodyheader">'+bodyheader1+'</h3><p class="bodycontent">'+bodycontent1+'</p></div><div class="body1 col-sm-6"><img class="img-responsive bodyimage" src="'+imageurl1+'"></div></div></div></div>')
+                
+            }
         }
         
         
@@ -97,22 +101,30 @@ $(document).ready(function(){
         }
         var style = document.getElementById("style").value;
         var videoid = document.getElementById("videoid").value;
-        var videoid = document.getElementById("videoid").value;
+        //HEADER
+        var title = document.getElementById("title").value;
+        $('.title').replaceWith('<title class="title">'+title+'</title>')
+        var keywords = document.getElementById("keywords").value;
+        $('.keywords').replaceWith('<meta class="keywords" name="keywords" content="'+keywords+'">')
+        var description = document.getElementById("description").value;
+        $('.description').replaceWith('<meta class="description" name="description" content="'+description+'">')
+        
         var virustotal = document.getElementById("virustotal").value;
+        var virustotalappender = "window.open('"+virustotal+"')";
         var downloadbutton = document.getElementById("downloadbutton").value;
             if (downloadbutton==="href") {
                 var href = document.getElementById("href").value;
-                var openclickappender = 'window.open("'+href+'")';
+                var openclickappender = "window.open('"+href+"')";
                 }
             else {
                 var openclickappender = document.getElementById("onclick").value;
                 }
         var viral = $('input[name="viral"]:checked').val();
         if (viral==="download") {
-            $('.viraltoggle').replaceWith(' <div class="viraltoggle"><div class="viralonoff download"><button type="button" class="btn btn-success btn-lg dlbttn">Click To Download</button><p class="virusscan">Virus Scan: <span style="color:green;font-weight: bold;">Clean</span> (See <a class="viruslink" href="'+virustotal+'" target="_blank">Here</a>)</p><p class="lastupdated">Last Updated on <span class="date"></span></p></div></div>')
+            $('.viraltoggle').replaceWith('<div class="viraltoggle"><div class="viralonoff download"><button type="button" class="btn btn-success btn-lg dlbttn" onclick="'+openclickappender+'">Click To Download</button><p class="virusscan">Virus Scan: <span style="color:green;font-weight: bold;">Clean</span> (See <span class="viruslink" onclick="'+virustotalappender+'">Here</span)</p><p class="lastupdated">Last Updated on <span class="date"></span></p></div></div>')
         }
         else {
-            $('.viraltoggle').replaceWith("<div class='viraltoggle'><div class='viralonoff'><button type='button' class='btn btn-success btn-lg dlbttn' onclick='"+openclickappender+"'>Click To Download</button><p class='virusscan'>Virus Scan: <span style='color:green;font-weight: bold;'>Clean</span> (See <a class='viruslink' href=''+virustotal+'' target='_blank'>Here</a>)</p><p class='lastupdated'>Last Updated on <span class='date'></span></p></div></div>")
+            $('.viraltoggle').replaceWith('<div class="viraltoggle"><div class="viralonoff"><button type="button" class="btn btn-success btn-lg dlbttn" onclick="'+openclickappender+'">Click To Download</button><p class="virusscan">Virus Scan: <span style="color:green;font-weight: bold;">Clean</span> (See <span class="viruslink" onclick="'+virustotalappender+'">Here</span>)</p><p class="lastupdated">Last Updated on <span class="date"></span></p></div></div>')
         }
         $('.video').replaceWith('<iframe class="video" width="550" height="365" src="//www.youtube.com/embed/'+videoid+'?autoplay=0&showinfo=0&controls=0&modestbranding=1" frameborder="0" allowfullscreen></iframe>')
         if ($('.lphead').length === 0) {
@@ -131,12 +143,80 @@ $(document).ready(function(){
         $('.source').hide();
         $('#blackbg').remove();
     })
+    
+    
+    
+    
+    
     $('#source').click(function(){
+        
+        $('.title').replaceWith('<title class="title">Get Source Code</title>')
         $(this).addClass('activebutton');
+        //DECLARATION OF VARIABLES
         var title = document.getElementById("title").value;
         var head = document.getElementById("head").value;
         var keywords = document.getElementById("keywords").value;
         var description = document.getElementById("description").value;
+        var style = document.getElementById("style").value;
+        var videoid = document.getElementById("videoid").value;
+        var viral = $('input[name="viral"]:checked').val();
+        var downloadbutton = document.getElementById("downloadbutton").value;
+            if (downloadbutton==="href") {
+                var href = document.getElementById("href").value;
+                var openclickappender = "window.open('"+href+"')";
+                }
+            else {
+                var openclickappender = document.getElementById("onclick").value;
+                }
+        var virustotal = document.getElementById("virustotal").value;
+        var virustotalappender = "window.open('"+virustotal+"')";
+        var headerone = document.getElementById("header").value;
+        var subtext = document.getElementById("subtext").value;
+        var bodyparagraph = document.getElementById("bodyparagraph").value;
+        var bodyheader2 = document.getElementById("bodyheader2").value;
+        var imageurl2 = document.getElementById("imageurl2").value;
+        var bodycontent2 = document.getElementById("bodycontent2").value;
+        var bodyheader1 = document.getElementById("bodyheader1").value;
+        var imageurl1 = document.getElementById("imageurl1").value;
+        var bodycontent1 = document.getElementById("bodycontent1").value;
+        if (bodyparagraph != 1) {
+                var bodyparas = '<div class="bodycontentdivs"><div class="bodycontent1"><div class="row"><div class="body1 col-sm-6"><h3 class="bodyheader">'+bodyheader1+'</h3><p class="bodycontent">'+bodycontent1+'</p></div><div class="body1 col-sm-6"><img class="img-responsive bodyimage" src="'+imageurl1+'"></div></div></div><div class="bodycontent2"><div class="row"><div class="body2 col-sm-6"><img class="img-responsive bodyimage" src="'+imageurl2+'"></div><div class="body2 col-sm-6"><h3 class="bodyheader">'+bodyheader2+'</h3><p class="bodycontent2">'+bodycontent2+'</p></div></div></div></div>'
+            }
+            else {
+                var bodyparas = '<div class="bodycontentdivs"><div class="bodycontent1"><div class="row"><div class="body1 col-sm-6"><h3 class="bodyheader">'+bodyheader1+'</h3><p class="bodycontent">'+bodycontent1+'</p></div><div class="body1 col-sm-6"><img class="img-responsive bodyimage" src="'+imageurl1+'"></div></div></div></div>'
+                
+            }       
+        //if href, then get onclock function for href, if not, then get onclick function supplied.
+        //add openclick like onclick="'+openclickappender+'"
+        var downloadbutton = document.getElementById("downloadbutton").value;
+            if (downloadbutton==="href") {
+                var href = document.getElementById("href").value;
+                var openclickappender = "window.open('"+href+"')";
+                }
+            else {
+                var openclickappender = document.getElementById("onclick").value;
+                }
+        //virus total appender. add to like onclick="'+virustotalappender+'"
+        var virustotal = document.getElementById("virustotal").value;
+        var virustotalappender = "window.open('"+virustotal+"')";
+        
+        //APPEND SOURCE replace head then append other
+        var doge = '<!DOCTYPE html><html><head class="head"><title class="title">'+title+'</title><meta name="viewport" content="width=device-width, initial-scale=1.0"><link href="css/bootstrap.css" rel="stylesheet"><script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script><script src="js/bootstrap.min.js"></script><script class="javascript lphead" src="js/general.js" type="text/javascript"></script><link class="stylesheet lphead" href="css/stylesheet-'+style+'.css" rel="stylesheet"><link class="sociallockercss lphead" href="css/jquery.op.sociallocker-'+style+'.css" rel="stylesheet"><script class="socialockerjs lphead"src="js/jquery.op.sociallocker.min.js"></script><link href="http://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet" type="text/css"><link href="http://fonts.googleapis.com/css?family=Lato:400,300italic" rel="stylesheet" type="text/css"><meta class="keywords" name="keywords" content="'+keywords+'"><meta class="description" name="description" content="'+description+'">'+head+' </head><body><div class="lp container light"><div class="body col-md-8 col-md-offset-2"><div class="header"><div class="row"><div class="jumbotron"><h1 id="headerh1">'+headerone+'</h1></div><div class="divider"></div></div></div><p class="lead centertext subtext" id="subtextp">'+subtext+'</p><div class="row videotoggle"><div class="videocontainer" style="margin-top:20px;margin-right: auto;margin-left:auto;"><iframe class="video" width="550" height="365" src="//www.youtube.com/embed/'+videoid+'?autoplay=0&showinfo=0&controls=0&modestbranding=1" frameborder="0" allowfullscreen></iframe></div></div><div class="viraltoggle"><div class="viralonoff '+viral+'"><button type="button" class="btn btn-success btn-lg dlbttn" onclick="'+openclickappender+'">Click To Download</button><p class="virusscan">Virus Scan: <span style="color:green;font-weight: bold;">Clean</span> (See <a class="viruslink" onclick="'+virustotalappender+'">Here</a>)</p><p class="lastupdated">Last Updated on <span class="date"></span></p></div>'+bodyparas+' </div><div class="divider"></div><p class="footer-left inline">Copyright 2014</p><p class="footer-right inline">Designed by: Carbonizon</p></div></div></div></body></html>'
+        console.log(doge)
+        $(".codeoutput").focus(function() {
+            var $this = $(this);
+            $this.select();
+        
+            // Work around Chrome's little problem
+            $this.mouseup(function() {
+                // Prevent further mouseup intervention
+                $this.unbind("mouseup");
+                return false;
+            });
+        });
+        
+        $('.codeoutput').replaceWith('<xmp class="codeoutput">'+doge+'</xmp>')
+
         $('#lp').removeClass('activebutton');
         $('#builder').removeClass('activebutton');
         $('.builder').hide();
@@ -144,12 +224,22 @@ $(document).ready(function(){
         $('.lphead').replaceWith('');
         $('.source').show();
         if ($('#blackbg').length === 0) {
-            $('head').append('<style id="blackbg">body {background-image: url("images/bg.png");overflow-x: hidden;}</style>')
+            $('head').append('<style id="blackbg">body {background-color: rgba(1,1,1,0.8);}</style>')
         }
         else {
             
         }
     })
+    
+    
+    
+    ikik
+    
+    
+    
+    
+    
+    
     //jQuery code which toggles mobile nav bar on click of a
     $(function() {
         $('.nav a').on('click', function(){ 
